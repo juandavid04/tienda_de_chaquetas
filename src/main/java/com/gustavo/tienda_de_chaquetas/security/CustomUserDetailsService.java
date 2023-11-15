5221fa85896f4a7c562c1c123ff06d3b5ad95bec
juandavidporrasgomez9 <juandavidporrasgomez9@gmail.com>
2023-11-14 23:25:20 -0500

    UserDetailsService

--- src/main/java/com/gustavo/tienda_de_chaquetas/security/CustomUserDetailsService.java
+++ src/main/java/com/gustavo/tienda_de_chaquetas/security/CustomUserDetailsService.java
@@ -1,11 +1,21 @@
 package com.gustavo.tienda_de_chaquetas.security;
 
 import com.gustavo.tienda_de_chaquetas.dao.api.PersonaRepository;
+import com.gustavo.tienda_de_chaquetas.model.Persona;
+import com.gustavo.tienda_de_chaquetas.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService  implements UserDetailsService {
@@ -18,9 +28,15 @@ public class CustomUserDetailsService  implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        UserEntity user = personaRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Username not found"));
-        return new User(user.getUsername(), user.getPassword(), mapRolesToAuthorities(user.getRoles()));
+    public UserDetails loadUserByUsername(String correo) throws UsernameNotFoundException {
+
+        Persona ejemploPersona = new Persona();
+        ejemploPersona.setCorreo(correo);
+        Example<Persona> example = Example.of(ejemploPersona);
+
+        Persona user = personaRepository.findOne(example).orElseThrow(() -> new UsernameNotFoundException("UserEmail not found"));
+
+        return new User(user.getCorreo(), user.getPassword(), mapRolesToAuthorities(user.getRoles()) );
     }
 
     private Collection<GrantedAuthority> mapRolesToAuthorities(List<Role> roles) {
